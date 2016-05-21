0727d19f3f56fbf42f83ccba9f6f152833a04b09
Buddha <sankarbala123@gmail.com>
2016-05-21 14:40:51 -0700

    Two Sum Problem LeetCode

--- src/com/array/TwoSum/TwoSum.java
+++ src/com/array/TwoSum/TwoSum.java
@@ -1,5 +1,25 @@
 package com.array.TwoSum;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class TwoSum {
+    public int[] twoSums(int[] nums, int target) {
+        int[] result = new int[2];
+        if(nums == null || nums.length < 2) {
+            return result;
+        }
+
+        Map<Integer, Integer> numIndexMap = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++) {
+            if(numIndexMap.containsKey(target - nums[i])) {
+                result[0] = numIndexMap.get(target - nums[i]);
+                result[1] = i;
+            }
+            numIndexMap.put(nums[i], i);
+        }
+
+        return result;
+    }
 
 }
--- src/com/array/TwoSum/TwoSumApp.java
+++ src/com/array/TwoSum/TwoSumApp.java
@@ -2,6 +2,14 @@ package com.array.TwoSum;
 
 public class TwoSumApp {
     public static void main(String args[]) {
-        System.out.println("Two Sum:");
+        TwoSum ts = new TwoSum();
+        int nums[] = {1, 2, 3, 4, 5, 6};
+        int target = 9;
+        int[] result = ts.twoSums(nums, target);
+        System.out.println("Two Sum: ");
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
     }
 }
